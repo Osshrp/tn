7a0816df1b35b29fc0f6848c985e30ca86bbe7ec
Osshrp <osshrp@gmail.com>
2017-03-10 19:20:39 +0300

    L3 some fixes

--- railway/rail_way.rb
+++ railway/rail_way.rb
@@ -5,14 +5,14 @@ require_relative 'route'
 route = RailWay::Route.new
 route.add_station(RailWay::Station.new("Петушки"))
 train = RailWay::Train.new("23", 10, { type: "passenger" }, route)
-puts train.wagons_quantity
+puts "Количество вагонов: #{train.wagons_quantity}"
 train.attach_wagon
-puts train.wagons_quantity
+puts "Количество вагонов: #{train.wagons_quantity}"
 train.detach_wagon
-puts train.wagons_quantity
-puts train.current_station.name
+puts "Количество вагонов: #{train.wagons_quantity}"
+puts "Текущая станция: #{train.current_station.name}"
 train.go
-puts train.current_station.name
-puts train.next_station
-puts train.previous_station
+puts "Текущая станция: #{train.current_station.name}"
+puts "Следующая станция: #{train.next_station}"
+puts "Предыдующая станция: #{train.next_station}"
 # station = RailWay::Station.new("Казань")
